3d5dfc1eca2da9415ff4c0bf76b227a0a89353cf
Bruno Spindola <bcspindola@gmail.com>
2026-04-25 17:33:58 -0300

    Optimize Code and Separate the functions in other file
    Signed-off-by: Bruno Spindola <bcspindola@gmail.com>

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-def get_todos(filepath="todos.txt"):
-    """
-    Read a text file and return the list of to-do items.
-    """
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos
-
-def write_todos(todos_arg, filepath="todos.txt"):
-    """ Write a to-do items list in the text file."""
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
+# from functions import get_todos, write_todos
+import functions
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -18,15 +8,15 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:]
 
-        todos = get_todos("todos.txt")
+        todos = functions.get_todos()
 
         todos.append(todo + '\n')
 
-        write_todos(todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith('show'):
 
-        todos = get_todos("todos.txt")
+        todos = functions.get_todos()
 
         # Interesting way to solve
         new_todos = [f"{index + 1}-{item.strip('\n')}" for index, item in enumerate(todos)]
@@ -44,12 +34,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos("todos.txt")
+            todos = functions.get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
         except ValueError:
             print("Your command is not valid. Try again.")
@@ -59,12 +49,12 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos("todos.txt")
+            todos = functions.get_todos()
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
             message = f"Todo {todo_to_remove} is complete, and remove from the list."
             print(message)
